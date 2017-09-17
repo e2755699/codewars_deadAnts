c3045c038e37cd00606f07cb5ae91c5b9872ee11
Dustin <e275566@gmail.com>
2017-09-17 16:29:14 +0800

    fail version 1

---
+++ codewars_deadAnts/DeadAntsTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace codewars_deadAnts
+{
+    [TestClass]
+    public class DeadAntsTest
+    {
+        [TestMethod]
+        public void Input_a_shouldReturn_one()
+        {
+            Assert.AreEqual(1, GetDeadAnts("a"));
+        }
+
+        private int GetDeadAnts(string deadRound)
+        {
+            var kata = new Kata();
+            int deadAnts = kata.Accum(deadRound);
+            return deadAnts;
+        }
+
+        [TestMethod]
+        public void Input_ant_shouldReturn_zero()
+        {
+            Assert.AreEqual(0, GetDeadAnts("ant"));
+        }
+
+        [TestMethod]
+        public void Input_an_should_Return_one()
+        {
+            Assert.AreEqual(1, GetDeadAnts("an"));
+        }
+
+        [TestMethod]
+        public void Input_aa_shouldReturn_twe()
+        {
+            Assert.AreEqual(2, GetDeadAnts("aa"));
+        }
+
+        [TestMethod]
+        public void Input_n_shouldReturn_one()
+        {
+            Assert.AreEqual(1, GetDeadAnts("n"));
+        }
+    }
+}
---
+++ codewars_deadAnts/Kata.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace codewars_deadAnts
+{
+    internal class Kata
+    {
+        public Kata()
+        {
+        }
+
+        public int Accum(string input)
+        {
+            if (input != "ant")
+            {
+                int head = 0;
+                foreach (var postion in input)
+                {
+                    if (postion == 'a')
+                    {
+                        head++;
+                    }
+                }
+                return head;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
